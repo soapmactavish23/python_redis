3f4973caf1fea9e40822a7800db8ef9eea283db9
soapmactavish23 <ricknogueira1231@gmail.com>
2026-06-10 20:16:10 -0300

    Conexão em Redis

---
+++ src/models/redis/settings/connection.py
@@ -0,0 +1,12 @@
+from redis import Redis
+
+class RedisConnectionHandler:
+    def __init__(self) -> None:
+        self.__redis_conn = None
+
+    def connect(self) -> Redis:
+        self.__redis_conn = Redis(host='localhost', port=6379, db=0)
+        return self.__redis_conn
+
+    def get_connection(self) -> Redis:
+        return self.__redis_conn
