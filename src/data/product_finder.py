56d56de57cba70f8a494ea5d938b045950f03214
soapmactavish23 <ricknogueira1231@gmail.com>
2026-06-19 21:33:15 -0300

    Tipos HTTP

--- src/data/product_finder.py
+++ src/data/product_finder.py
@@ -1,3 +1,5 @@
+from src.http_types.http_request import HttpRequest
+from src.http_types.http_response import HttpResponse
 from src.models.redis.repository.interfaces.redis_repository_interface import RedisRepositoryInterface
 from src.models.sqlite.repository.products_repository_interface import ProductRepositoryInterface
 
@@ -6,3 +8,6 @@ class ProductFinder:
     def __init__(self, redis_repo: RedisRepositoryInterface, products_repo: ProductRepositoryInterface) -> None:
         self.__redis_repo = redis_repo
         self.__sqlite_repo = products_repo
+
+    def find_by_name(self, http_request: HttpRequest) -> HttpResponse:
+        pass
