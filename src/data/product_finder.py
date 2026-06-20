a7a90a0161161df896ac7bdfc38288e2944830c3
soapmactavish23 <ricknogueira1231@gmail.com>
2026-06-19 21:42:18 -0300

    Lógica de Busca com Cache

--- src/data/product_finder.py
+++ src/data/product_finder.py
@@ -8,6 +8,46 @@ class ProductFinder:
     def __init__(self, redis_repo: RedisRepositoryInterface, products_repo: ProductRepositoryInterface) -> None:
         self.__redis_repo = redis_repo
         self.__sqlite_repo = products_repo
+        self.__product_repo = products_repo
 
     def find_by_name(self, http_request: HttpRequest) -> HttpResponse:
-        pass
+        product_name = http_request.params["product_name"]
+        product = self.__find_in_cache(product_name)
+        if not product:
+            product = self.__find_in_sql(product_name)
+            self.__insert_in_cache(product)
+
+        return self.__format_response(product)
+
+    def __find_in_cache(self, product_name: str) -> tuple:
+        product_infos = self.__redis_repo.get_key(product_name)
+        if product_infos:
+            product_infos_list = product_infos.split(",")
+            return (0, product_name, product_infos_list[0], product_infos_list[1])
+
+        return None
+
+    def __find_in_sql(self, product_name: str) -> tuple:
+        product = self.__product_repo.find_product_by_name(product_name)
+        if not product: raise Exception("Produto não encontrado!")
+
+        return product
+
+    def __insert_in_cache(self, product: tuple) -> None:
+        product_name = product[1]
+        value = f"{product[2]},{product[3]}"
+        self.__redis_repo.insert_ex(product_name, value, ex=60)
+
+    def __format_response(self, product: tuple) -> HttpResponse:
+        return HttpResponse(
+            status_code=200,
+            body={
+                "type": "PRODUCT",
+                "count": 1,
+                "attributes": {
+                    "name": product[1],
+                    "price": product[2],
+                    "quantity": product[3],
+                }
+            }
+        )
