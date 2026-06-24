12e749f4e36d794c345d32ee867625c40f94952a
soapmactavish23 <ricknogueira1231@gmail.com>
2026-06-23 22:20:56 -0300

    Finalizando Rotas

--- src/main/routes/products_routes.py
+++ src/main/routes/products_routes.py
@@ -1,11 +1,23 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+
+from src.http_types.http_request import HttpRequest
+from src.main.composer.product_creator_composer import product_create_composer
+from src.main.composer.product_finder_composer import product_finder_composer
 
 products_routes_bp = Blueprint('products_routes_bp', __name__)
 
 @products_routes_bp.route('/products', methods=['POST'])
 def insert_product():
-    return jsonify({"msg": "produto cadastrado"}), 200
+    http_request = HttpRequest(body=request.json)
+    product_creator = product_create_composer()
+    http_response = product_creator.create(http_request)
+
+    return jsonify(http_response.body), http_response.status_code
 
 @products_routes_bp.route('/products/<product_name>', methods=['GET'])
 def get_product(product_name: str):
-    return jsonify({"msg": f"produto {product_name}"}), 200
+    http_request = HttpRequest(params={'product_name': product_name})
+    product_finder = product_finder_composer()
+    http_response = product_finder.find_by_name(http_request)
+
+    return jsonify(http_response.body), http_response.status_code
